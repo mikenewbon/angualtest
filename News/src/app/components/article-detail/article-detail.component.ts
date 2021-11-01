a31c00f0b089e65ebfd4f8a494b4f77054f38cc0
Mike <carnfall@gmail.com>
2021-11-02 00:48:32 +0100

    Add link home

--- News/src/app/components/article-detail/article-detail.component.ts
+++ News/src/app/components/article-detail/article-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from 'src/app/services/article.service';
-import { Article } from '../../Article';
 
 @Component({
   selector: 'app-article-detail',
